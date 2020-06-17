0e206f5fa80a1be504ab561d32faa6ad89933ecd
Tim Rabl <tim.rabl@hotmail.com>
2020-06-17 23:52:49 +0200

    fixed permissions in Dockerfile

--- image-files/docker-entrypoint.sh
+++ image-files/docker-entrypoint.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
 if [ ! -e /etc/apache2/certs/server.key ] && [ ! -e /etc/apache2/certs/server.crt ]; then
-	openssl req -newkey rsa:4096 -new -nodes -x509 -days ${SSL_EXPIRE} -keyout /etc/apache2/certs/server.key -out /etc/apache2/certs/server.crt -subj "/C=${SSL_C}/ST=${SSL_ST}/L=${SSL_L}/O=${SSL_O}/OU=${SSL_OU}/CN=${SSL_CN}"
+	openssl req \
+        -newkey rsa:4096 \
+        -new \
+        -nodes \
+        -x509 \
+        -days ${SSL_EXPIRE} \
+        -keyout /etc/apache2/certs/server.key \
+        -out /etc/apache2/certs/server.crt \
+        -subj "/C=${SSL_C}/ST=${SSL_ST}/L=${SSL_L}/O=${SSL_O}/OU=${SSL_OU}/CN=${SSL_CN}"
 fi
 
-chown apache:apache /var/www/localhost/htdocs/data /php_sessions
+#chown apache:apache /var/www/localhost/htdocs/data /etc/apache2/php_sessions
 
 rm -f /run/apache2/httpd.pid 2>/dev/null
 exec httpd -DFOREGROUND
